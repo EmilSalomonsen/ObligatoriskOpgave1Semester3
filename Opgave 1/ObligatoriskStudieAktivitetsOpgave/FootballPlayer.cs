1b0b0374673199e57676f0fcfa99d8ae69a83997
EmilSalomonsen <73826248+EmilSalomonsen@users.noreply.github.com>
2021-09-30 10:33:59 +0200

    Add files via upload
    Opgave 1

---
+++ Opgave 1/ObligatoriskStudieAktivitetsOpgave/FootballPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ObligatoriskStudieAktivitetsOpgave
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string _name;
+        public int _price;
+        public int _shirtnumber;
+        public string Name
+        { 
+            get => _name; 
+            set
+            {
+                if (value.Length < 4) throw new ArgumentException("Name has to be 4 or more characters long");
+                    _name = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Price has to be over 0");
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtnumber;
+            set
+            {
+                if (value <= 1 || value >= 100) throw new ArgumentException("Shirt number has to be between 1 and 100");
+                _shirtnumber = value;
+            }
+        }
+
+    }
+}
---
+++ Opgave 1/ObligatoriskStudieAktivitetsOpgaveTests/FootballPlayerTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskStudieAktivitetsOpgave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatoriskStudieAktivitetsOpgave.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void IdTest()
+        {
+            FootballPlayer footballplayer = new FootballPlayer();
+            footballplayer.Id = 1;
+            Assert.AreEqual(1, footballplayer.Id);
+        }
+        [TestMethod()]
+        public void NameTest()
+        {
+            //Method works
+            FootballPlayer footballPlayer = new FootballPlayer();
+            footballPlayer.Name = "Emil";
+            Assert.AreEqual("Emil", footballPlayer.Name);
+
+            Assert.ThrowsException<ArgumentException>(
+                () => footballPlayer.Name = "Bo");
+
+            //Method doesn't work
+            //FootballPlayer footballPlayer1 = new FootballPlayer();
+            //footballPlayer1.Name = "Bo";
+            //Assert.AreEqual("Bo", footballPlayer1.Name);
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            // Method works
+            FootballPlayer footballplayer = new FootballPlayer();
+            footballplayer.Price = 1;
+            Assert.AreEqual(1, footballplayer.Price);
+            Assert.ThrowsException<ArgumentException>(
+            () => footballplayer.Price = 0);
+
+            //Method doesn't work
+            //FootballPlayer footballplayer1 = new FootballPlayer();
+            //footballplayer1.Price = 0;
+            //Assert.AreEqual(0, footballplayer1.Price);
+        }
+
+        [TestMethod()]
+        public void ShirtNumberTest()
+        {
+            //Method Works
+            FootballPlayer footballplayer = new FootballPlayer();
+            footballplayer.ShirtNumber = 10;
+            Assert.AreEqual(10, footballplayer.ShirtNumber);
+            Assert.ThrowsException<ArgumentException>(
+            () => footballplayer.ShirtNumber= -10);
+
+            //Method doesn't work
+            //FootballPlayer footballplayer1 = new FootballPlayer();
+            //footballplayer1.ShirtNumber = 101;
+            //Assert.AreEqual(101, footballplayer1.ShirtNumber);
+
+            //Method doesn't work
+            //FootballPlayer footballplayer2 = new FootballPlayer();
+            //footballplayer2.ShirtNumber = 0;
+            //Assert.AreEqual(0, footballplayer2.ShirtNumber);
+
+
+        }
+
+
+    }
+}
+
+namespace ObligatoriskStudieAktivitetsOpgaveTests
+{
+    class FootballPlayerTests
+    {
+    }
+}
